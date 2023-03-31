bc79d4562a1f1889dc24a664eeb053e1b2037122
vince-portfolio <84810206+vince-portfolio@users.noreply.github.com>
2023-03-31 08:18:23 -0400

    Add files via upload
    Signed-off-by: vince-portfolio <84810206+vince-portfolio@users.noreply.github.com>

--- fighterspaygap_data - 2022 comparison.js
+++ fighterspaygap_data - 2022 comparison.js
@@ -1,29 +1,29 @@
 const at=
 `athlete name,sport,image,net worth,net worth pos,2022 sport earnings,earnings pos,2022 appearances,2022 earnings per appearance,earn per app pos,2022 minutes,2022 earnings per minute,earn per min pos
-Conor McGregor,MMA,mcgregor.png,"$200,000,000",1 ⭐️,"$33,000,000",11 ⭐️,1,"$33,000,000",2 ⭐️,1,"$33,000,000",1 ⭐️
-Tyson Fury,Boxing,fury.png,"$62,000,000",12 ⭐️,"$60,000,000",6 ⭐️,2,"$30,000,000",3 ⭐️,48,"$1,250,000",2 ⭐️
-Canelo Alvarez,Boxing,alvarez.png,"$90,000,000",8 ⭐️,"$85,000,000",1 ⭐️,2,"$42,500,000",1 ⭐️,72,"$1,180,556",3 ⭐️
-Matthew Stafford,Football,stafford.png,"$72,300,000",10 ⭐️,"$69,800,000",4 ⭐️,9,"$7,755,556",5 ⭐️,85,"$821,176",4 ⭐️
-Aaron Rodgers,Football,rodgers.png,"$68,000,000",11 ⭐️,"$57,000,000",7 ⭐️,17,"$3,352,941",6 ⭐️,85,"$670,588",5 ⭐️
-Jake Paul,Boxing,paul.png,"$38,000,000",13 ⭐️,"$30,000,000",13 ⭐️,1,"$30,000,000",4 ⭐️,48,"$625,000",6 ⭐️
-Tom Brady,Football,brady.png,"$83,900,000",9 ⭐️,"$31,900,000",12 ⭐️,17,"$1,876,471",9 ⭐️,85,"$375,294",7 ⭐️
-Dustin Poirier,MMA,poirier.png,"$10,000,000",15 ⭐️,"$1,671,000",17 ⭐️,1,"$1,671,000",10 ⭐️,12,"$139,250",8 ⭐️
-Michael Chandler,MMA,chandler.png,"$2,000,000",20 ⭐️,"$1,629,000",18 ⭐️,2,"$814,500",17 ⭐️,17.28,"$94,271",9 ⭐️
-Aljamain Sterling,MMA,sterling.png,"$2,000,000",22 ⭐️,"$2,409,000",16 ⭐️,2,"$1,204,500",14 ⭐️,28.73,"$83,850",10 ⭐️
-Israel Adesanya,MMA,adesanya.png,"$10,000,000",14 ⭐️,"$5,956,000",14 ⭐️,3,"$1,985,333",8 ⭐️,72.02,"$82,699",11 ⭐️
-Khamzat Chimaev,MMA,chimaev.png,"$1,000,000",26 ⭐️,"$1,390,500",19 ⭐️,2,"$695,250",21 ⭐️,17.22,"$80,749",12 ⭐️
-Alexander Volkanovski,MMA,volkanovski.png,"$5,000,000",19 ⭐️,"$2,974,000",15 ⭐️,2,"$1,487,000",11 ⭐️,40.75,"$72,982",13 ⭐️
-Nate Diaz,MMA,diaz.png,"$9,000,000",16 ⭐️,"$971,000",22 ⭐️,1,"$971,000",16 ⭐️,17.87,"$54,337",14 ⭐️
-Amanda Nunes,MMA,nunes.png,"$8,000,000",17 ⭐️,"$1,032,000",21 ⭐️,1,"$1,032,000",15 ⭐️,25,"$41,280",15 ⭐️
-Brandon Moreno,MMA,moreno.png,"$2,000,000",21 ⭐️,"$1,324,000",20 ⭐️,2,"$662,000",22 ⭐️,39.57,"$33,460",16 ⭐️
-Islam Makhachev,MMA,makhachev.png,"$1,000,000",23 ⭐️,"$743,000",23 ⭐️,2,"$371,500",25 ⭐️,29.27,"$25,384",17 ⭐️
-Neymar,Soccer,neymar.png,"$95,000,000",5 ⭐️,"$70,000,000",3 ⭐️,32,"$2,187,500",7 ⭐️,2797,"$25,027",18 ⭐️
-Max Holloway,MMA,holloway.png,"$6,000,000",18 ⭐️,"$532,000",24 ⭐️,1,"$532,000",23 ⭐️,25,"$21,280",19 ⭐️
-Stephen Curry,Basketball,curry.png,"$92,800,000",6 ⭐️,"$45,800,000",8 ⭐️,64,"$715,625",20 ⭐️,2211,"$20,715",20 ⭐️
-Kevin Durant,Basketball,durant.png,"$92,100,000",7 ⭐️,"$42,100,000",9 ⭐️,55,"$765,455",18 ⭐️,2047,"$20,567",21 ⭐️
-LeBron James,Basketball,james.png,"$121,200,000",3 ⭐️,"$41,200,000",10 ⭐️,56,"$735,714",19 ⭐️,2084,"$19,770",22 ⭐️
-Paddy Pimblett,MMA,pimblett.png,"$1,000,000",25 ⭐️,"$432,000",26 ⭐️,2,"$216,000",26 ⭐️,22.77,"$18,972",23 ⭐️
-Cristiano Ronaldo,Soccer,ronaldo.png,"$115,000,000",4 ⭐️,"$60,000,000",5 ⭐️,46,"$1,304,348",13 ⭐️,3457,"$17,356",24 ⭐️
-Lionel Messi,Soccer,messi.png,"$130,000,000",2 ⭐️,"$75,000,000",2 ⭐️,51,"$1,470,588",12 ⭐️,4470,"$16,779",25 ⭐️
-Leon Edwards,MMA,edwards.png,"$1,000,000",24 ⭐️,"$432,000",25 ⭐️,1,"$432,000",24 ⭐️,29.04,"$14,876",26 ⭐️
+Conor McGregor,MMA,mcgregor.png,$200m,1 ⭐️,$33m,11 ⭐️,1,$33m,2 ⭐️,1,$33m,1 ⭐️
+Tyson Fury,Boxing,fury.png,$62m,12 ⭐️,$60m,6 ⭐️,2,$30m,3 ⭐️,48,$1.25m,2 ⭐️
+Dustin Poirier,MMA,poirier.png,$10m,15 ⭐️,$1.671m,17 ⭐️,1,$1.671m,10 ⭐️,12,$139.25k,8 ⭐️
+Canelo Alvarez,Boxing,alvarez.png,$90m,8 ⭐️,$85m,1 ⭐️,2,$42.5m,1 ⭐️,72,$1.180556m,3 ⭐️
+Michael Chandler,MMA,chandler.png,$2m,20 ⭐️,$1.629m,18 ⭐️,2,$814.5k,17 ⭐️,17.28,"$94,271",9 ⭐️
+Matthew Stafford,Football,stafford.png,$72.3m,10 ⭐️,$69.8m,4 ⭐️,9,$7.755556m,5 ⭐️,85,"$821,176",4 ⭐️
+Aljamain Sterling,MMA,sterling.png,$2m,22 ⭐️,$2.409m,16 ⭐️,2,$1.2045m,14 ⭐️,28.73,$83.85k,10 ⭐️
+Aaron Rodgers,Football,rodgers.png,$68m,11 ⭐️,$57m,7 ⭐️,17,$3.352941m,6 ⭐️,85,"$670,588",5 ⭐️
+Israel Adesanya,MMA,adesanya.png,$10m,14 ⭐️,$5.956m,14 ⭐️,3,$1.985333m,8 ⭐️,72.02,"$82,699",11 ⭐️
+Jake Paul,Boxing,paul.png,$38m,13 ⭐️,$30m,13 ⭐️,1,$30m,4 ⭐️,48,$625k,6 ⭐️
+Khamzat Chimaev,MMA,chimaev.png,$1m,26 ⭐️,$1.3905m,19 ⭐️,2,$695.25k,21 ⭐️,17.22,"$80,749",12 ⭐️
+Tom Brady,Football,brady.png,$83.9m,9 ⭐️,$31.9m,12 ⭐️,17,$1.876471m,9 ⭐️,85,"$375,294",7 ⭐️
+Alexander Volkanovski,MMA,volkanovski.png,$5m,19 ⭐️,$2.974m,15 ⭐️,2,$1.487m,11 ⭐️,40.75,"$72,982",13 ⭐️
+Neymar,Soccer,neymar.png,$95m,5 ⭐️,$70m,3 ⭐️,32,$2.1875m,7 ⭐️,2797,"$25,027",18 ⭐️
+Nate Diaz,MMA,diaz.png,$9m,16 ⭐️,$971k,22 ⭐️,1,$971k,16 ⭐️,17.87,"$54,337",14 ⭐️
+Stephen Curry,Basketball,curry.png,$92.8m,6 ⭐️,$45.8m,8 ⭐️,64,"$715,625",20 ⭐️,2211,"$20,715",20 ⭐️
+Amanda Nunes,MMA,nunes.png,$8m,17 ⭐️,$1.032m,21 ⭐️,1,$1.032m,15 ⭐️,25,$41.28k,15 ⭐️
+Kevin Durant,Basketball,durant.png,$92.1m,7 ⭐️,$42.1m,9 ⭐️,55,"$765,455",18 ⭐️,2047,"$20,567",21 ⭐️
+Brandon Moreno,MMA,moreno.png,$2m,21 ⭐️,$1.324m,20 ⭐️,2,$662k,22 ⭐️,39.57,$33.46k,16 ⭐️
+LeBron James,Basketball,james.png,$121.2m,3 ⭐️,$41.2m,10 ⭐️,56,"$735,714",19 ⭐️,2084,$19.77k,22 ⭐️
+Islam Makhachev,MMA,makhachev.png,$1m,23 ⭐️,$743k,23 ⭐️,2,$371.5k,25 ⭐️,29.27,"$25,384",17 ⭐️
+Cristiano Ronaldo,Soccer,ronaldo.png,$115m,4 ⭐️,$60m,5 ⭐️,46,$1.304348m,13 ⭐️,3457,"$17,356",24 ⭐️
+Max Holloway,MMA,holloway.png,$6m,18 ⭐️,$532k,24 ⭐️,1,$532k,23 ⭐️,25,$21.28k,19 ⭐️
+Lionel Messi,Soccer,messi.png,$130m,2 ⭐️,$75m,2 ⭐️,51,$1.470588m,12 ⭐️,4470,"$16,779",25 ⭐️
+Paddy Pimblett,MMA,pimblett.png,$1m,25 ⭐️,$432k,26 ⭐️,2,$216k,26 ⭐️,22.77,"$18,972",23 ⭐️
+Leon Edwards,MMA,edwards.png,$1m,24 ⭐️,$432k,25 ⭐️,1,$432k,24 ⭐️,29.04,"$14,876",26 ⭐️
 `
